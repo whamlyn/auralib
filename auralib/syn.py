b165f3c734cd42d11f18ac178996618ba1ea2385
Wes Hamlyn <wes.hamlyn@gmail.com>
2021-02-06 16:53:41 -0700

    Added examples for creating log-based synthetics.

--- auralib/syn.py
+++ auralib/syn.py
@@ -18,13 +18,13 @@ def d2t(depth, data, td_depth, td_twt, target_twt):
     time-depth pairs.  This function both converts from depth to time and
     resamples the log data to a regular sampling rate in time.
     """
-    
+
     f1 = interp1d(td_depth, td_twt, kind='linear', bounds_error=False, fill_value=np.nan)
     twt_logstep = f1(depth)
-        
+
     f2 = interp1d(twt_logstep, data, kind='linear', bounds_error=False, fill_value=np.nan)
     data_twt = f2(target_twt)
-    
+
     return data_twt
 
 
@@ -35,13 +35,13 @@ def t2d(twt, data, td_depth, td_twt, target_depth):
     time-depth pairs.  This function both converts from time to depth and
     resamples the log data to a regular sampling rate in depth.
     """
-    
+
     f1 = interp1d(td_twt, td_depth, kind='cubic', bounds_error=False, fill_value='extrapolate')
     depth_timestep = f1(twt)
-        
+
     f2 = interp1d(depth_timestep, data, kind='cubic', bounds_error=False, fill_value='extrapolate')
     data_depth = f2(target_depth)
-    
+
     return data_depth
 
 
@@ -49,29 +49,29 @@ def t2d(twt, data, td_depth, td_twt, target_depth):
 def backus_length(f_dom, Vs0, mode=1):
     """
     Returns the backus average length for scattering and transmission limits.
-    
+
     L = backus_length(f_dom, Vs0, mode=1)
-    
+
     if mode==1:
         L = Ls = scattering limit (reflection seismic)
-        
+
     if mode==2:
         L = Lt = transmission limit (ray tracing)
 
     """
-    
+
     Vs0 = float(Vs0)
     f_dom = float(f_dom)
-    
+
     Ls = Vs0/(3.0*f_dom)
     Lt = 2.0*Vs0/f_dom
-    
+
     if mode == 1:
         L = Ls
-            
+
     if mode == 2:
         L = Lt
-    
+
     return L
 
 
@@ -79,116 +79,102 @@ def backus_length(f_dom, Vs0, mode=1):
 def backus_average(vp, vs, rho, nsamp):
     """
     Computes the backus average from vp, vs, and density logs.
-    
+
     Vpv, Vph, Vsv, Vsh, rho = backus_average(vp, vs, rho, nsamp)
     """
-    
+
     # make sure number of samples is odd
     nsamp = np.round(nsamp)
     if nsamp == 0:
         nsamp = 1
     if nsamp%2.0 == 0:
         nsamp = int(np.ceil(nsamp))
-    
+
     print('Backus averaging over %i samples' % nsamp)
-    
+
     filt = np.ones(nsamp)
-    
+
     # pad top and bottom of input logs to avoid filtering edge effects.  Do
     # this by duplicating the first and last sample in the vp, vs, and rho logs
     # by ~1/2 the Backus filtering length
     pad_length = int((nsamp-1)/2)
-        
+
     vp = np.hstack([np.ones(pad_length)*vp[0], vp, np.ones(pad_length)*vp[-1]])
     vs = np.hstack([np.ones(pad_length)*vs[0], vs, np.ones(pad_length)*vs[-1]])
     rho = np.hstack([np.ones(pad_length)*rho[0], rho, np.ones(pad_length)*rho[-1]])
-    
+
     mu = rho*(vs**2.0)
     K = rho*(vp**2.0) - 4.0/3.0*mu
     lame = K - 2.0/3.0*mu
-    
-    
+
+
     A1 = (4.0*mu*(lame+mu))/(lame+2.0*mu)
     A2 = (1.0/(lame+2.0*mu))**-1.0
     A3 = (lame/(lame+2.0*mu))**2.0
-    
+
     A1 = np.convolve(filt, A1, mode='same')/nsamp
     A2 = np.convolve(filt, A2, mode='same')/nsamp
     A3 = np.convolve(filt, A3, mode='same')/nsamp
     A = A1 + A2*A3
-    
+
     B1 = (2.0*mu*lame)/(lame+2.0*mu)
     B2 = (1.0/(lame+2.0*mu))**-1.0
     B3 = (lame/(lame+2.0*mu))**2.0
-    
+
     B1 = np.convolve(filt, B1, mode='same')/nsamp
     B2 = np.convolve(filt, B2, mode='same')/nsamp
     B3 = np.convolve(filt, B3, mode='same')/nsamp
     B = B1 + B2*B3
-    
+
     C = (1.0/(lame+2.0*mu))**-1.0
     C = np.convolve(filt, C, mode='same')/nsamp
-    
+
     F1 = (1.0/(lame+2.0*mu))**-1.0
     F2 = lame/(lame+2.0*mu)
-    
+
     F1 = np.convolve(filt, F1, mode='same')/nsamp
     F2 = np.convolve(filt, F2, mode='same')/nsamp
     F = F1*F2
-    
+
     D = (1.0/mu)**-1.0
     D = np.convolve(filt, D, mode='same')/nsamp
-    
+
     M = np.convolve(filt, mu, mode='same')/nsamp
-    
+
     rho_b = np.convolve(filt, rho, mode='same')/nsamp
-    
+
     Vpv_b = np.sqrt(C/rho_b)
     Vph_b = np.sqrt(A/rho_b)
     Vsv_b = np.sqrt(D/rho_b)
     Vsh_b = np.sqrt(M/rho_b)
-    
+
     # remove padding added to avoid edge effects
     Vpv_b = Vpv_b[pad_length:-pad_length]
     Vph_b = Vph_b[pad_length:-pad_length]
     Vsv_b = Vsv_b[pad_length:-pad_length]
     Vsh_b = Vsh_b[pad_length:-pad_length]
     rho_b = rho_b[pad_length:-pad_length]
-    
+
     return Vpv_b, Vph_b, Vsv_b, Vsh_b, rho_b
 
 
 
-def plot_wigva(ax, tdata, t, baseline=0, peak=True, trough=True, 
-          lcolor='k', pcolor=[0.2, 0.2, 1.0], tcolor=[1.0, 0.2, 0.2]):
+def plot_wigva(ax, tdata, twt, excursion=1.0, repeat=3, c='k', lw=0.5, peak=True,
+               trough=True, pcolor='blue', tcolor='red', alpha=0.3):
     """
-    Python function to plot synthetic wiggle traces with variable area fill.
+    Plot a single seismic trace in wiggle-variable area format.
     """
-    
-    from scipy.interpolate import interp1d
-    
-    # to make the peak/trough fill more accurate near zero-crossings, increase
-    # the sampling of synthetic traces by a factor of 5
-    nsamp = len(tdata)
-    t2 = np.linspace(t.min(), t.max(), nsamp*5)
-    
-    # interpolate the new trace
-    #t2 = np.arange(t.min(), t.max(), dt2)
-    f = interp1d(t, tdata, kind='linear')
-    trc2 = f(t2)
-    
-    if (peak == True) | (trough == True):
-        for i in range(0, 1):
-            # plot peak fill
-            if peak==True:
-                ax.fill_betweenx(t2, baseline, trc2, where=trc2>=baseline, 
-                                 alpha=0.6, facecolor=pcolor,  edgecolor=pcolor)
-            
-            # plot trough fill
-            if trough==True:
-                ax.fill_betweenx(t2, baseline, trc2, where=trc2<=baseline, 
-                                 alpha=0.6, facecolor=tcolor,  edgecolor=tcolor)
-    
-        ax.plot(trc2, t2, lcolor)
 
+    tdata_norm = tdata / np.max(np.abs(tdata))
+    for i in range(repeat):
+        baseline = 1.0*i
+        tdata_plt = tdata_norm*excursion + baseline
+
+        if peak==True:
+            ax.fill_betweenx(twt, baseline, tdata_plt, where=tdata_plt>=baseline,
+                                fc=pcolor, alpha=alpha, interpolate=True)
+        if trough==True:
+            ax.fill_betweenx(twt, baseline, tdata_plt, where=tdata_plt<=baseline,
+                                fc=tcolor, alpha=alpha, interpolate=True)
+        ax.plot(tdata_plt, twt, c=c, lw=lw)
 
---
+++ examples/create_synthetic_zero_offset_with_seismic_tie.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 17:09:07 2021
+
+@author: wesha
+"""
+
+#%%
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import MultiCursor
+import auralib as aura
+from scipy.interpolate import interp1d
+
+# load vp-vs-rho logs
+infile = r'C:\Users\wesha\Dropbox\data\well.las'
+
+buf = aura.las.LASReader(infile)
+md = buf.curves['DEPT']
+vp = 1e6/buf.curves['AC_GGG']
+rho = buf.curves['DEN'] * 0.001
+
+# 1) Strip leading and trailing nulls from Vp and Rho logs
+
+leading_vp = np.argmin(np.isnan(vp))
+leading_vp_idx = np.arange(leading_vp)
+vp1 = np.delete(vp, leading_vp_idx)
+
+trailing_vp = np.argmin(np.isnan(vp[::-1]))
+trailing_vp_idx = np.arange(-trailing_vp, 0)
+vp1 = np.delete(vp1, trailing_vp_idx)
+vp1z = np.delete(np.delete(md, leading_vp_idx), trailing_vp_idx)
+
+leading_rho = np.argmin(np.isnan(rho))
+leading_rho_idx = np.arange(leading_rho)
+rho1 = np.delete(rho, leading_rho_idx)
+
+trailing_rho = np.argmin(np.isnan(rho[::-1]))
+trailing_rho_idx = np.arange(-trailing_rho, 0)
+rho1 = np.delete(rho1, trailing_rho_idx)
+rho1z = np.delete(np.delete(md, leading_rho_idx), trailing_rho_idx)
+
+
+# 2) Resample logs such that they now cover the same Z-range
+
+z_min = np.max([vp1z[0], rho1z[0]])
+z_max = np.min([vp1z[-1], rho1z[-1]])
+dz = 0.1
+z = np.arange(z_min, z_max+dz, dz)
+
+# note: The below interpolators ignore any remaining nulls meaning that any
+#       gaps in the vp and rho logs will be interpolated. This may or may not
+#       be your desired behaviour, particuarly if large gaps are present.
+fvp = interp1d(vp1z[~np.isnan(vp1)], vp1[~np.isnan(vp1)], kind='linear',
+               bounds_error=False, fill_value=(vp1[0], vp1[-1]))
+
+frho= interp1d(rho1z[~np.isnan(rho1)], rho1[~np.isnan(rho1)], kind='linear',
+               bounds_error=False, fill_value=(rho1[0], rho1[-1]))
+
+vp_syn = fvp(z)
+rho_syn = frho(z)
+
+ai_syn = vp_syn*rho_syn
+sm_len_samp = 51
+ai_syn_sm = aura.utils.smooth_log(ai_syn, sm_len_samp)
+
+# do some despiking of the AI log
+if True:
+    spike_tol = 1000
+    idx = np.nonzero((ai_syn > ai_syn_sm+spike_tol) | (ai_syn < ai_syn_sm-spike_tol))
+    ai_syn[idx] = np.nan
+
+    f = interp1d(z[~np.isnan(ai_syn)], ai_syn[~np.isnan(ai_syn)], kind='linear',
+                 bounds_error=False, fill_value='extrapolate')
+    ai_syn = f(z)
+    ai_syn_sm = aura.utils.smooth_log(ai_syn, sm_len_samp)
+
+
+# 3) Calculate the initial time-depth transform from the vp_syn log
+
+twt_dz = np.cumsum(dz/(vp_syn))*2
+dt = 0.002
+twt_dt = np.arange(twt_dz.min(), twt_dz.max(), dt)
+
+
+# 4) Convert log digits from depth sampling to time sampling. this makes use
+#    of the Depth2Time() class
+
+class Depth2Time():
+    """
+    Class for depth to time conversion
+    """
+    def __init__(self, twt_dz, twt_dt):
+        """
+        Establish relation beteen depth-sampled and time-sampled twt values
+        """
+        self.twt_dz = twt_dz
+        self.twt_dt = twt_dt
+
+    def conv(self, data_dz):
+        """
+        Resample depth logs to twt
+        """
+        f = interp1d(self.twt_dz, data_dz, kind='linear', bounds_error=True)
+        data_dt = f(self.twt_dt)
+
+        return data_dt
+
+d2t = Depth2Time(twt_dz, twt_dt)
+
+vp_syn_t  = d2t.conv(vp_syn)
+rho_syn_t = d2t.conv(rho_syn)
+ai_syn_t  = d2t.conv(ai_syn)
+ai_syn_sm_t  = d2t.conv(ai_syn_sm)
+
+
+# 5) Compute zero offset reflection coefficients and add a zero to the start of
+#    the reflectivity arrays to account for no reflection at the top of the
+#    first layer.
+
+rpp_syn_sm_t = (ai_syn_sm_t[1:] - ai_syn_sm_t[:-1])/(ai_syn_sm_t[1:] + ai_syn_sm_t[:-1])
+rpp_syn_sm_t = np.hstack([0, rpp_syn_sm_t])
+
+
+# 6) Create a wavelet
+f1 = 5.0
+f2 = 10.0
+f3 = 100.0
+f4 = 125.0
+phase = 0.0
+wvlt_length_samp = 75
+wvlt_length_sec = (wvlt_length_samp-1)*dt
+wvlt_t, wvlt_a = aura.wvlt.wvlt_bpass(f1, f2, f3, f4, phase, dt, wvlt_length_sec)
+
+wvlt_a = wvlt_a * aura.win.cosine(wvlt_length_samp, 10)
+
+
+# 7) Convolve wavelet with reflectivity series
+
+synth = np.convolve(wvlt_a, rpp_syn_sm_t, mode='same')
+
+
+# 8) Read in a trace from SEG-Y file for time-depth calibration
+
+sgyfile = r'C:\Users\wesha\Dropbox\data\SEGY\seismic.sgy'
+def_thead = {'il':{'bpos':9,  'fmt':'l', 'nbyte':4},
+             'xl':{'bpos':13, 'fmt':'l', 'nbyte':4},
+             'cmpx':{'bpos':81, 'fmt':'f', 'nbyte':4},
+             'cmpy':{'bpos':85, 'fmt':'f', 'nbyte':4}}
+
+sgybuf = aura.segy.Segy(sgyfile, def_thead)
+sgybuf.fmt_str = '>f'
+
+trcnum = sgybuf.get_ilxl(100, 100)[0]
+tdata = sgybuf.read_tdata(trcnum)
+tdata_twt = np.arange(sgybuf.bhead['num_samp'])*sgybuf.bhead['samp_rate']*1e-6
+
+
+# 9)  Calibrate time-depth relationship
+
+well_twt_shift = 0.242
+twt_dt = twt_dt + well_twt_shift
+twt_dz = twt_dz + well_twt_shift
+
+# set to True to perform stretch-squeeze
+twt_orig = np.array([0.404, 0.4990, 0.519, 0.563, 0.640]) # original twt points
+twt_new =  np.array([0.397, 0.5005, 0.518, 0.560, 0.650]) # after stretch squeeze
+
+if True:
+
+    deltaT = twt_new - twt_orig  # time shifts at tie points
+
+    # create interpolator to compute time-shifts over full length of
+    f = interp1d(twt_orig, deltaT, kind='linear', bounds_error=False,
+                 fill_value=(deltaT[0], deltaT[-1]))
+    DeltaT_dt = f(twt_dt)
+    DeltaT_dz = f(twt_dz)
+
+    twt_dt = twt_dt + DeltaT_dt
+    twt_dz = twt_dz + DeltaT_dz
+    twt_orig = twt_orig + deltaT
+
+
+#
+# PLOTTING CODE BELOW...
+#
+
+fig = plt.figure(num=1)
+fig.clf()
+
+nr = 1; nc = 7
+ax = [plt.subplot2grid((nr, nc), (0, 0))]
+for i in range(1, nc):
+    ax.append(plt.subplot2grid((nr, nc), (0, i), sharey=ax[0]))
+
+# plot original AI logs in TWT but sampled in Depth
+ax[0].plot(ai_syn, twt_dz, c='k', lw=0.75)
+ax[0].plot(ai_syn_sm, twt_dz, c='r', lw=2)
+ax[0].set_xlim(3000, 10000)
+ax[0].set_xlabel('AI\n(m/s*g/cc)')
+
+# plot upscaled AI log sampled in TWT
+ax[1].step(ai_syn_sm_t, twt_dt, where='pre', c='r')
+ax[1].set_xlim(3000, 10000)
+ax[1].set_xlabel('AI\n(m/s*g/cc)')
+
+# plot reflectivity series
+ax[2].hlines(twt_dt, 0.0, rpp_syn_sm_t, 'r')
+ax[2].plot(np.zeros_like(twt_dt), twt_dt, 'r')
+ax[2].set_xlim(-0.5, 0.5)
+ax[2].set_xlabel('Refl. Coeff.\n(unitless)')
+
+# plot wavelet
+wvlt_t2 = wvlt_t + 0.5
+aura.syn.plot_wigva(ax[3], wvlt_a, wvlt_t2, repeat=1)
+ax[3].set_xlabel('Wavelet\n(amplitude)')
+
+# plot synthetic trace (replicated several times)
+aura.syn.plot_wigva(ax[4], synth, twt_dt, repeat=5)
+xlim = ax[4].get_xlim()
+for each in twt_new:
+    ax[4].plot(xlim, [twt_orig, twt_orig], lw=2)
+ax[4].set_xlim(xlim)
+ax[4].set_xlabel('Synthetic\n(amplitude)')
+
+# plot seismic trace (replicated several times)
+aura.syn.plot_wigva(ax[5], tdata, tdata_twt, repeat=5)
+xlim = ax[5].get_xlim()
+for each in twt_new:
+    ax[5].plot(xlim, [twt_new, twt_new], lw=2)
+ax[5].set_xlim(xlim)
+ax[5].set_xlabel('Seismic\n(amplitude)')
+
+ax[6].plot(DeltaT_dt, twt_dt)
+ax[6].set_xlabel('delta TWT\n(seconds)')
+
+for each in ax:
+    each.invert_yaxis()
+
+aura.plot.format_log_axes(ax, '')
+ax[0].set_ylim(0.7, 0.3)
+
+curs = MultiCursor(fig.canvas, ax, horizOn=True, vertOn=False, c='k', ls='-', lw=1)
+plt.show()
